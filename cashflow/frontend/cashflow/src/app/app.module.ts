865273a52b1dc8d93bcf32eca2fc6238c70ed4cc
avmari <mariya.avdeeva251001@gmail.com>
2022-05-14 17:15:38 +0400

    admin component

--- cashflow/frontend/cashflow/src/app/app.module.ts
+++ cashflow/frontend/cashflow/src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AdministratorComponent } from './administrator/administrator.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    AdministratorComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
